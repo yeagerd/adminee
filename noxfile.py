182a457a878bbc9762ba52ad135d6cb5abea6387
Dan Yeager <>
2025-07-08 21:15:29 -0700

    Add missing dep and fix nox venv issue

--- noxfile.py
+++ noxfile.py
@@ -5,36 +5,36 @@ nox.options.sessions = [
 ]
 nox.options.reuse_existing_virtualenvs = True
 
-@nox.session
+@nox.session(venv_backend="none")
 def format(session):
     session.run("black", "--check", "--diff", "services/", external=True)
     session.run("isort", "--check-only", "--diff", "services/", external=True)
 
-@nox.session
+@nox.session(venv_backend="none")
 def lint(session):
     session.run("ruff", "check", "services/", external=True)
 
-@nox.session
+@nox.session(venv_backend="none")
 def typecheck(session):
     session.run("bash", "-c", "cd services/user && python -m mypy . --ignore-missing-imports --no-strict-optional --exclude='alembic/'", external=True)
     session.run("bash", "-c", "cd services/chat && python -m mypy . --ignore-missing-imports --no-strict-optional --exclude='alembic/'", external=True)
     session.run("bash", "-c", "cd services/office && python -m mypy . --ignore-missing-imports --no-strict-optional --exclude='alembic/'", external=True)
 
-@nox.session
+@nox.session(venv_backend="none")
 def typecheck_strict(session):
     session.run("mypy", "services", "--strict", external=True)
 
-@nox.session
+@nox.session(venv_backend="none")
 def test(session):
     session.run("pytest", "services/", "-n", "auto", "-q", "--tb=short", external=True)
 
-@nox.session
+@nox.session(venv_backend="none")
 def test_fast(session):
     session.run("bash", "-c", "cd services/user && python -m pytest tests/ -x -q", external=True)
     session.run("bash", "-c", "cd services/chat && python -m pytest tests/ -x -q", external=True)
     session.run("bash", "-c", "cd services/office && python -m pytest tests/ -x -q", external=True)
 
-@nox.session
+@nox.session(venv_backend="none")
 def test_cov(session):
     session.run("bash", "-c", "cd services/user && python -m pytest tests/ --cov=. --cov-report=xml:../../coverage-user-management.xml --cov-report=term-missing -x -q", external=True)
     session.run("bash", "-c", "cd services/chat && python -m pytest tests/ --cov=. --cov-report=xml:../../coverage-chat-service.xml --cov-report=term-missing -x -q", external=True)
