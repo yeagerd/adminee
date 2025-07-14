e49b6e89cf7d222d85743fd930b89fd6020715e7
Dan Yeager <>
2025-07-14 15:23:32 -0700

    resolved nav bar being below main page

--- frontend/components/ui/sidebar.tsx
+++ frontend/components/ui/sidebar.tsx
@@ -219,21 +219,10 @@ const Sidebar = React.forwardRef<
                 data-variant={variant}
                 data-side={side}
             >
-                {/* This is what handles the sidebar gap on desktop */}
                 <div
                     className={cn(
-                        "duration-200 relative h-svh w-[--sidebar-width] bg-transparent transition-[width] ease-linear",
-                        "group-data-[collapsible=offcanvas]:w-0",
-                        "group-data-[side=right]:rotate-180",
-                        variant === "floating" || variant === "inset"
-                            ? "group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)_+_theme(spacing.4))]"
-                            : "group-data-[collapsible=icon]:w-[--sidebar-width-icon]"
-                    )}
-                />
-                <div
-                    className={cn(
-                        // CHANGED: fixed -> relative, h-svh -> h-full, removed inset-y-0
-                        "duration-200 relative z-10 hidden h-full w-[--sidebar-width] transition-[left,right,width] ease-linear md:flex",
+                        // REMOVED: z-10
+                        "duration-200 relative hidden h-full w-[--sidebar-width] transition-[left,right,width] ease-linear md:flex",
                         side === "left"
                             ? "left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]"
                             : "right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]",
@@ -247,7 +236,7 @@ const Sidebar = React.forwardRef<
                 >
                     <div
                         data-sidebar="sidebar"
-                        className="flex h-full w-full flex-col bg-sidebar group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:border-sidebar-border group-data-[variant=floating]:shadow"
+                        className="flex h-full w-full flex-col items-start justify-start bg-sidebar group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:border-sidebar-border group-data-[variant=floating]:shadow"
                     >
                         {children}
                     </div>
