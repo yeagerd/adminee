6db207b95957c0bdf6c792a78cbf14879b34b0a8
Dan Yeager <>
2025-08-13 08:00:36 -0700

    style: apply code formatting fixes from nox

--- services/office/tests/test_api_contacts.py
+++ services/office/tests/test_api_contacts.py
@@ -48,10 +48,10 @@ class TestContactsApi:
             class FakeGoogle:
                 async def __aenter__(self):
                     return self
-                
+
                 async def __aexit__(self, exc_type, exc_val, exc_tb):
                     pass
-                
+
                 async def get_contacts(self, page_size=200):
                     return {
                         "connections": [
@@ -73,10 +73,10 @@ class TestContactsApi:
             class FakeMS:
                 async def __aenter__(self):
                     return self
-                
+
                 async def __aexit__(self, exc_type, exc_val, exc_tb):
                     pass
-                
+
                 async def get_contacts(self, top=200, select=None, order_by=None):
                     return {
                         "value": [
