a72735a1701df81b7f4600a45281bc8213b7c81b
Dan Yeager <>
2025-08-13 00:50:59 -0700

    one-line

--- frontend/components/ui/input.tsx
+++ frontend/components/ui/input.tsx
@@ -5,9 +5,9 @@ import * as React from 'react';
 export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 export const Input = React.forwardRef<HTMLInputElement, InputProps>(function Input({ className = '', ...props }, ref) {
-  return (
-    <input ref={ref} className={`border rounded px-2 py-1 ${className}`} {...props} />
-  );
+    return (
+        <input ref={ref} className={`border rounded px-2 py-1 ${className}`} {...props} />
+    );
 });
 
 export default Input;
