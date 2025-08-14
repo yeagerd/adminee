7527f865810a39bbd3e5a974b0c6e2f3248578c0
Dan Yeager <>
2025-08-14 13:32:36 -0700

    refactor: move field-update-message to ui/

--- frontend/components/email/track-shipment-modal.tsx
+++ frontend/components/email/track-shipment-modal.tsx
@@ -1,8 +1,8 @@
-import FieldUpdateMessage from '@/components/general/field-update-message';
 import { Alert, AlertDescription } from '@/components/ui/alert';
 import { Button } from '@/components/ui/button';
 import { Checkbox } from '@/components/ui/checkbox';
 import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import FieldUpdateMessage from '@/components/ui/field-update-message';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
