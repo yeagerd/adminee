08ce786e1231011e0169d70a572c3956671a426f
Dan Yeager <997983+yeagerd@users.noreply.github.com>
2025-08-19 08:48:45 -0700

    Remove generated OpenAPI schemas and TypeScript types from version control
    - Add generated files to .gitignore
    - Remove schema.json files from all services
    - Remove generated TypeScript types from frontend/types/api/
    - These files will be regenerated in CI/CD or local development

--- frontend/types/api/meetings/models/MeetingType.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export enum MeetingType {
-    IN_PERSON = 'in_person',
-    VIRTUAL = 'virtual',
-    TBD = 'tbd',
-}
--- frontend/types/api/shipments/models/LabelUpdate.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type LabelUpdate = {
-    name: (string | null);
-    color: (string | null);
-};
-
