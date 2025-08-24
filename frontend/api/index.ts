c2968c6d2e06acc2d9ff882e953ff2740ebdc95c
Dan Yeager <997983+yeagerd@users.noreply.github.com>
2025-08-24 13:33:43 -0700

    Update API index: remove officeApi, add contactsApi export

--- frontend/api/index.ts
+++ frontend/api/index.ts
@@ -1,9 +1,9 @@
 // Export base classes
 export { BookingsClient } from './clients/bookings-client';
 export { ChatClient } from './clients/chat-client';
+export { ContactsClient } from './clients/contacts-client';
 export { MeetingsClient } from './clients/meetings-client';
 export { OfficeClient } from './clients/office-client';
-
 export { UserClient } from './clients/user-client';
 
 // Export types from clients
@@ -18,13 +18,14 @@ export * from './clients/shipments-client';
 // Export service instances for convenience
 import { BookingsClient } from './clients/bookings-client';
 import { ChatClient } from './clients/chat-client';
+import { ContactsClient } from './clients/contacts-client';
 import { MeetingsClient } from './clients/meetings-client';
 import { OfficeClient } from './clients/office-client';
-
 import { ShipmentsClient } from './clients/shipments-client';
 import { UserClient } from './clients/user-client';
+
 export const bookingsApi = new BookingsClient();
-export const officeApi = new OfficeClient();
+export const contactsApi = new ContactsClient();
 export const chatApi = new ChatClient();
 export const userApi = new UserClient();
 export const meetingsApi = new MeetingsClient();
