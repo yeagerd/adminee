33e3386d226c9c7b70733a00a1154f1b1b052576
Dan Yeager <997983+yeagerd@users.noreply.github.com>
2025-08-24 11:55:38 -0700

    Update generated files

--- frontend/types/api/meetings/models/PollResponseTokenRequest.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-import type { PollResponseCreate } from './PollResponseCreate';
-export type PollResponseTokenRequest = {
-    responses: Array<PollResponseCreate>;
-};
-
--- frontend/types/api/vespa_loader/index.ts
+++
@@ -1,7 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type { HTTPValidationError } from './models/HTTPValidationError';
-export type { ValidationError } from './models/ValidationError';
