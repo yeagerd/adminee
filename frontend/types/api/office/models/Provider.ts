befac29fd75b6e1ade8dafa5b65cfebc9ad3b974
Dan Yeager <997983+yeagerd@users.noreply.github.com>
2025-08-27 11:44:31 -0700

    Update generated files

--- frontend/types/api/office/models/Provider.ts
+++ frontend/types/api/office/models/Provider.ts
@@ -2,6 +2,9 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
+/**
+ * Provider enumeration for office integrations.
+ */
 export enum Provider {
     GOOGLE = 'google',
     MICROSOFT = 'microsoft',
